f2ad2d416c2f2d4c5257981f8db06a9bdf794595
AmeliaTYR <53657436+AmeliaTYR@users.noreply.github.com>
2021-07-25 15:53:49 +0800

    Create basic gameobjects
    - created ground
    - created player

---
+++ PushyGame/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem; // for inputs
+using TMPro; // for text
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 10;
+    private Rigidbody rb;
+    private float movementX;
+    private float movementY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+    }
+
+    public void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
+
+    public void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+
+        rb.AddForce(movement * speed);
+    }
+
+    // called by unity when the player entity first touches a trigger collider and is given a reference
+    // to the trigger collider that will be touched
+
+}
