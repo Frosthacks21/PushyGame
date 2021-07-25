611dfc5e782e669d82a75750a90bda53f05c7cfc
AmeliaTYR <53657436+AmeliaTYR@users.noreply.github.com>
2021-07-25 16:07:51 +0800

    Create Prefabs and secondary platform

---
+++ PushyGame/Assets/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 offset;
+    // private cause we set the value in script
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // offset calculated immediately but only needs to be done once
+        offset = transform.position - player.transform.position;
+    }
+
+    // Change to late update so that the camera position only change after the player position
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
